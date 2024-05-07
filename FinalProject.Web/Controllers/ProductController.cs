74a1e3c48e0505720387ba76c838b19b69d4653c
ParvizHajili <parviz.hajili@gmail.com>
2024-05-07 16:52:19 +0400

    Add project files.

---
+++ FinalProject.Web/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using FinalProject.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+           List<Car> cars = new()
+           {
+               new Car { Id = 1,Name="Hyundai"},
+               new Car { Id = 2,Name="Bmw"},
+               new Car { Id = 3,Name="Mercedes"}
+           };
+
+            return View(cars);
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ FinalProject.Web/Models/Car.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalProject.Web.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
